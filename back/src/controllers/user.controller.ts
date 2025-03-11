ffe79284c10ab2e5429b338116b0eeb6a9a112a9
antonincmp <antonin.campi@epitech.eu>
2025-03-11 15:31:02 +0100

    RM: useless route

--- back/src/controllers/user.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const getUsers = (req: Request, res: Response) => {
-    res.json([{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]);
-};
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes";
 import connectDB from "./config/db";
 import { setupSwagger } from "./config/swagger";
 import authRoutes from "./routes/auth.routes";
@@ -24,5 +23,4 @@ app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
 
-app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
