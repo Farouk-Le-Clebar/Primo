995a4d598dd5f92ed75201417d5aa9043be185ea
Louis Huguet <louis.huguet@epitech.eu>
2025-03-11 15:23:21 +0100

    ADD : display info component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { DisplayParcelleInfoComponent } from './map/display-parcelle-info/display-parcelle-info.component';
 
 export const routes: Routes = [
     { path: 'map', component: MapComponent },
+    { path: 'display', component: DisplayParcelleInfoComponent },
 ];
