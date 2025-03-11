28321b46411d69a1b8cdd4c0f3b2170d30271a07
Louis Huguet <louis.huguet@epitech.eu>
2025-03-11 12:07:31 +0100

    ADD : open steet maps

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
 
 export const routes: Routes = [
-    { path: '', component: MapComponent },
+    { path: 'map', component: MapComponent },
 ];
--- web/src/app/map/map.component.ts
+++ web/src/app/map/map.component.ts
@@ -4,17 +4,19 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+
+  constructor() { }
+
   ngOnInit(): void {
     const map = L.map('map', {
       center: [51.505, -0.09],
       zoom: 13
     });
 
-    L.tileLayer('https://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://stamen.com">Stamen Design</a>, <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     }).addTo(map);
   }
 }
