54b645e9726a12740d6162d83e12c4f873b313d6
Masse Elyne <elyne.masse@epitech.eu>
2025-03-11 15:16:50 +0100

    ADD : frontend login page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'map', component: MapComponent },
+    { path: 'login', component: LoginComponent },
 ];
