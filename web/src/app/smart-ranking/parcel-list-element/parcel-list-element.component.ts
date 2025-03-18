5fb3d792374248fc555bdc085078ea3fae158006
antonincmp <antonin.campi@epitech.eu>
2025-03-18 11:14:38 +0100

    ADD: smart ranking page

--- web/src/app/smart-ranking/parcel-list-element/parcel-list-element.component.ts
+++ web/src/app/smart-ranking/parcel-list-element/parcel-list-element.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parcel-list-element',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './parcel-list-element.component.html',
   styleUrl: './parcel-list-element.component.scss'
 })
 export class ParcelListElementComponent {
-
+  @Input() parcelId: string = 'Parcelle n° 75105 AB 0123';
+  @Input() price: string = '100 €/m²';
+  @Input() starCount: number = 5;
 }
--- web/src/app/smart-ranking/smart-ranking.component.ts
+++ web/src/app/smart-ranking/smart-ranking.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ParcelListElementComponent } from './parcel-list-element/parcel-list-element.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-smart-ranking',
-  imports: [NavbarComponent, ParcelListElementComponent],
+  standalone: true,
+  imports: [NavbarComponent, ParcelListElementComponent, CommonModule],
   templateUrl: './smart-ranking.component.html',
-  styleUrl: './smart-ranking.component.scss'
+  styleUrls: ['./smart-ranking.component.scss']
 })
 export class SmartRankingComponent {
+  parcels = [
+    { parcelId: 'Parcelle n° 75105 AB 0123', price: '246 €/m²', starCount: 0 },
+    { parcelId: 'Parcelle n° 75014 AA 0456', price: '170 €/m²', starCount: 0 },
+    { parcelId: 'Parcelle n° 69003 AC 0789', price: '100 €/m²', starCount: 0 },
+    { parcelId: 'Parcelle n° 13008 AD 0321', price: '90 €/m²', starCount: 0 }
+  ];
 
+  generateRanking() {
+    console.log('Generating ranking...');
+    const rankingValues = [5, 5, 4, 2];
+    this.parcels.forEach((parcel, index) => {
+      parcel.starCount = rankingValues[index];
+    });
+  }
 }
