f71f13afef96e31639276be36fca7addcda06896
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-03-19 10:19:51 +0100

    ADD: new style of poc

--- web/src/app/navbar/navbar.component.ts
+++ web/src/app/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private router: Router) { }
 
+  onSearchClick() {
+    this.router.navigate(['/search']);
+  }
+
+  onMapClick() {
+    this.router.navigate(['/map']);
+  }
+
+  onFavoritesClick() {
+    this.router.navigate(['/favorit']);
+  }
+
+  onHomeClick() {
+    this.router.navigate(['/']);
+  }
 }
