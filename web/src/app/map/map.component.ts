5445efda53fb250531f1e92f7bee37acf4c99047
Louis Huguet <louis.huguet@epitech.eu>
2025-03-19 19:49:33 +0100

    ADD : highlight parcelle on map

--- web/src/app/map/map.component.ts
+++ web/src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DisplayParcelleInfoComponent } from './display-parcelle-info/display-parcelle-info.component';
 import * as L from 'leaflet';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -18,8 +19,10 @@ export class MapComponent implements OnInit {
   private geojsonParcelles: any = null;
   private geojsonBatiments: any = null;
   private selectedParcelles: { [key: string]: L.Polygon } = {};
+  private highlightedIds = [""];
+  zoomAnimation: boolean = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.map = L.map('map', {
@@ -35,25 +38,29 @@ export class MapComponent implements OnInit {
       touchZoom: false
     });
 
+    this.route.queryParams.subscribe(params => {
+      this.zoomAnimation = params['zoomAnimation'] === 'true';
+    });
+
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {}).addTo(this.map);
 
     this.parcelleLayerGroup = L.layerGroup().addTo(this.map);
     this.batimentLayerGroup = L.layerGroup().addTo(this.map);
-    this.loadPolygons();
 
-    setTimeout(() => {
-      this.map.on('tileload', () => {
-        console.log('Tile loaded');
-      });
-    
-      this.map.whenReady(() => {
-        this.map.flyTo([43.65583714209635, 0.5733537389936271], 18, {
-          duration: 3,
+    if (this.zoomAnimation) {
+      setTimeout(() => {
+        this.map.whenReady(() => {
+          this.map.flyTo([43.65583714209635, 0.5733537389936271], 18, {
+            duration: 3,
+          });
         });
-      });
-    }, 1000);
+      }, 1000);
+      this.highlightedIds = ['32013000BL0091', '32013000BL0164', '32013000BN0122', '32013000BL0151'];
+    } else {
+      this.map.setView([43.65583714209635, 0.5733537389936271], 18);
+    }
 
-    fetch('cadastre-32013-parcelles.json')
+    const parcellesPromise = fetch('cadastre-32013-parcelles.json')
       .then(response => response.json())
       .then(data => {
         if (data && data.features) {
@@ -61,10 +68,9 @@ export class MapComponent implements OnInit {
         } else {
           console.error('Aucune donnée GeoJSON disponible pour les parcelles');
         }
-      })
-      .catch(error => console.error('Erreur lors de la récupération des données GeoJSON des parcelles:', error));
+      });
 
-    fetch('cadastre-32013-batiments.json')
+    const batimentsPromise = fetch('cadastre-32013-batiments.json')
       .then(response => response.json())
       .then(data => {
         if (data && data.features) {
@@ -72,13 +78,20 @@ export class MapComponent implements OnInit {
         } else {
           console.error('Aucune donnée GeoJSON disponible pour les bâtiments');
         }
-      })
-      .catch(error => console.error('Erreur lors de la récupération des données GeoJSON des bâtiments:', error));
+      });
+
+    Promise.all([parcellesPromise, batimentsPromise]).then(() => {
+      this.loadPolygons();
+    });
   }
 
   private loadPolygons(): void {
-    this.map.on('moveend', () => this.updateVisibleLayers());
-    this.map.on('zoomend', () => this.updateVisibleLayers());
+    if (this.zoomAnimation) {
+      this.map.on('moveend', () => this.updateVisibleLayers());
+      this.map.on('zoomend', () => this.updateVisibleLayers());
+    } else {
+      this.updateVisibleLayers();
+    }
   }
 
   private updateVisibleLayers(): void {
@@ -88,40 +101,58 @@ export class MapComponent implements OnInit {
 
   private updateVisibleParcelles(): void {
     if (!this.geojsonParcelles) return;
-  
+
+    console.log("caca");
     this.parcelleLayerGroup.clearLayers();
     const bounds = this.map.getBounds();
-  
+
     const visibleFeatures = this.geojsonParcelles.features.filter((feature: any) => {
       const [minLng, minLat, maxLng, maxLat] = L.geoJSON(feature).getBounds().toBBoxString().split(',').map(Number);
       return bounds.intersects([[minLat, minLng], [maxLat, maxLng]]);
     });
-  
+
+
     const visibleParcelles = L.geoJSON(visibleFeatures, {
-      style: (feature) => feature && feature.properties && this.selectedParcelles[feature.properties.id] ? this.getHoverStyle() : this.getDefaultStyle(),
+      style: (feature) => this.getFeatureStyle(feature?.properties.id),
       onEachFeature: (feature, layer: L.Layer) => {
         if (feature.properties && feature.properties.id && layer instanceof L.Polygon) {
           const polygonLayer = layer as L.Polygon;
-          const parcelleId = feature.properties.id;
-  
-          polygonLayer.setStyle(this.getDefaultStyle());
-  
+
           polygonLayer.on('mouseover', () => {
-            if (!this.selectedParcelles[parcelleId]) polygonLayer.setStyle(this.getHoverStyle());
+            if (!this.selectedParcelles[feature.properties.id]) {
+              polygonLayer.setStyle(this.getFeatureStyle(feature.properties.id, true));
+            }
           });
-  
+
           polygonLayer.on('mouseout', () => {
-            if (!this.selectedParcelles[parcelleId]) polygonLayer.setStyle(this.getDefaultStyle());
+            if (!this.selectedParcelles[feature.properties.id]) {
+              polygonLayer.setStyle(this.getFeatureStyle(feature.properties.id));
+            }
           });
-  
-          polygonLayer.on('click', () => this.onParcelleClick(parcelleId, polygonLayer));
+
+          polygonLayer.on('click', () => this.onParcelleClick(feature.properties.id, polygonLayer));
         }
       }
     });
-  
+
     visibleParcelles.addTo(this.parcelleLayerGroup);
   }
 
+
+  private getFeatureStyle(id: string, isSelected: boolean = false) {
+    if (!isSelected) {
+      if (id && this.highlightedIds.includes(id)) {
+        return { color: '#FFD700', weight: 2, opacity: 1, fillOpacity: 0.2 };
+      }
+      return { color: '#8067B7', weight: 1, opacity: 1, fillOpacity: 0 };
+    } else {
+      if (id && this.highlightedIds.includes(id)) {
+        return { color: '#FFD700', weight: 3, opacity: 1, fillOpacity: 0.6 };
+      }
+      return { color: '#FFFFFF', weight: 2, opacity: 1, fillOpacity: 0.4 };
+    }
+  }
+  
   private updateVisibleBatiments(): void {
     if (!this.geojsonBatiments) return;
 
@@ -150,28 +181,20 @@ export class MapComponent implements OnInit {
     visibleBatiments.addTo(this.batimentLayerGroup);
   }
 
-  private getDefaultStyle() {
-    return { color: '#8067B7', weight: 1, opacity: 1, fillOpacity: 0 };
-  }
-
-  private getHoverStyle() {
-    return { color: '#FFFFFF', weight: 2, opacity: 1, fillOpacity: 0.2 };
-  }
-
   private onParcelleClick(parcelleId: string, polygonLayer: L.Polygon): void {
     if (!this.selectedParcelles[parcelleId]) {
       for (const id in this.selectedParcelles) {
         if (this.selectedParcelles.hasOwnProperty(id)) {
-          this.selectedParcelles[id].setStyle(this.getDefaultStyle());
+          this.selectedParcelles[id].setStyle(this.getFeatureStyle(id, false));
           delete this.selectedParcelles[id];
         }
       }
       this.selectedParcelles[parcelleId] = polygonLayer;
-      polygonLayer.setStyle(this.getHoverStyle());
+      polygonLayer.setStyle(this.getFeatureStyle(parcelleId, true));
       this.parcelleNbr = parcelleId;
     } else {
       delete this.selectedParcelles[parcelleId];
-      polygonLayer.setStyle(this.getDefaultStyle());
+      polygonLayer.setStyle(this.getFeatureStyle(parcelleId, false));
       this.parcelleNbr = '';
     }
   }
