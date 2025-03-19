b40c2390e0627893e34bbd77c55426ac8c17b9f9
Louis Huguet <louis.huguet@epitech.eu>
2025-03-19 12:01:01 +0100

    ADD : zoom animation on map

--- web/src/app/map/map.component.ts
+++ web/src/app/map/map.component.ts
@@ -24,7 +24,7 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.map = L.map('map', {
       center: [43.65583714209635, 0.5733537389936271],
-      zoom: 50,
+      zoom: 10,
       zoomControl: false,
       attributionControl: false,
       dragging: false,
@@ -40,14 +40,27 @@ export class MapComponent implements OnInit {
     this.parcelleLayerGroup = L.layerGroup().addTo(this.map);
     this.batimentLayerGroup = L.layerGroup().addTo(this.map);
 
+    this.map.on('tileload', () => {
+      console.log('Tile loaded');
+    });
+
+    this.map.whenReady(() => {
+      this.map.flyTo([43.65583714209635, 0.5733537389936271], 18, {
+        duration: 3,
+      });
+    });
+
+    this.map.on('zoomend', () => {
+      if (this.map.getZoom() === 18) {
+        this.loadPolygons();
+      }
+    });
+
     fetch('cadastre-32013-parcelles.json')
       .then(response => response.json())
       .then(data => {
         if (data && data.features) {
           this.geojsonParcelles = data;
-          this.updateVisibleLayers();
-          this.map.on('moveend', () => this.updateVisibleLayers());
-          this.map.on('zoomend', () => this.updateVisibleLayers());
         } else {
           console.error('Aucune donnée GeoJSON disponible pour les parcelles');
         }
@@ -59,12 +72,18 @@ export class MapComponent implements OnInit {
       .then(data => {
         if (data && data.features) {
           this.geojsonBatiments = data;
-          this.updateVisibleLayers();
         } else {
           console.error('Aucune donnée GeoJSON disponible pour les bâtiments');
         }
       })
       .catch(error => console.error('Erreur lors de la récupération des données GeoJSON des bâtiments:', error));
+  }
+
+  private loadPolygons(): void {
+    this.updateVisibleLayers();
+
+    this.map.on('moveend', () => this.updateVisibleLayers());
+    this.map.on('zoomend', () => this.updateVisibleLayers());
   }
 
   private updateVisibleLayers(): void {
