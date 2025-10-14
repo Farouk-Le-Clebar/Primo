b24a92a091063c2877c957835838c38613eb9f5d
Louis Huguet <louis.huguet@epitech.eu>
2025-10-14 16:28:29 +0200

    ADD : optional option on navbar and footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Search from "./pages/search/Search";
 
 function App() {
+  const location = useLocation();
+
+  const showNavbarOn = ["/"];
+
+  const shouldShowNavbar = showNavbarOn.some((p) => {
+    if (p === "/") return location.pathname === "/";
+    return location.pathname === p || location.pathname.startsWith(p + "/");
+  });
+
   return (
     <>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
-      <Footer />
+      {shouldShowNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      {shouldShowNavbar && <Footer />}
     </>
   );
 }
