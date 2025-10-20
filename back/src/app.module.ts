0c468bd699161f6142f4242a0b580049663412a9
Garoverr <jacques.sapin@epitech.eu>
2025-10-20 17:08:14 +0200

    ADD: install script create graphhopper data install the france OpenStreetMap file

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { User } from './database/user.entity';
 import { AddokProxyMiddleware } from './api/addok.middleware';
 import { ApicartoProxyMiddleware } from './api/apicarto.middleware';
+import { GraphhopperProxyMiddleware } from './api/graphhopper.middleware';
 
 @Module({
   imports: [
@@ -33,5 +34,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AddokProxyMiddleware).forRoutes('/addok');
     consumer.apply(ApicartoProxyMiddleware).forRoutes('/apicarto');
+    consumer.apply(GraphhopperProxyMiddleware).forRoutes('/gh');
   }
 }
