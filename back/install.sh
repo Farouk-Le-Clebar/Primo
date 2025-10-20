3416d36a6af308ea08a384185e6996b096a6e873
Garoverr <jacques.sapin@epitech.eu>
2025-10-20 17:54:50 +0200

    FIX: replace france.osm.pbf by occitanie due to oversizing

--- back/install.sh
+++ back/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# wget https://adresse.data.gouv.fr/data/ban/adresses/latest/addok/addok-france-bundle.zip
+wget https://adresse.data.gouv.fr/data/ban/adresses/latest/addok/addok-france-bundle.zip
 
-# mkdir addok-data
-# unzip -d addok-data addok-france-bundle.zip
+mkdir addok-data
+unzip -d addok-data addok-france-bundle.zip
 
-# rm -rf addok-france-bundle.zip
+rm -rf addok-france-bundle.zip
 
 mkdir graphhopper-data
-wget https://download.geofabrik.de/europe/france-latest.osm.pbf -O graphhopper-data/france.osm.pbf
+wget https://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf -O graphhopper-data/occitanie.osm.pbf
