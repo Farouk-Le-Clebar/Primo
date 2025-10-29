9aa90ef41a71837b12a2c26befe2254e9afdc6f8
Garoverr <jacques.sapin@epitech.eu>
2025-10-29 14:45:40 +0100

    FIX: for rebase

--- back/install.sh
+++ back/install.sh
@@ -7,5 +7,49 @@ unzip -d addok-data addok-france-bundle.zip
 
 rm -rf addok-france-bundle.zip
 
-mkdir graphhopper-data
-wget https://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf -O graphhopper-data/occitanie.osm.pbf
+
+
+wget https://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf -O gh-routing/gh-data/occitanie/occitanie.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/alsace-latest.osm.pbf -O gh-routing/gh-data/alsace/alsace.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/aquitaine-latest.osm.pbf -O gh-routing/gh-data/aquitaine/aquitaine.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf -O gh-routing/gh-data/bretagne/bretagne.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf -O gh-routing/gh-data/auvergne/auvergne.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/basse-normandie-latest.osm.pbf -O gh-routing/gh-data/basse-normandie/basse-normandie.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/haute-normandie-latest.osm.pbf -O gh-routing/gh-data/haute-normandie/haute-normandie.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/centre-latest.osm.pbf -O gh-routing/gh-data/centre/centre.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/champagne-ardenne-latest.osm.pbf -O gh-routing/gh-data/champagne-ardenne/champagne-ardenne.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/franche-comte-latest.osm.pbf -O gh-routing/gh-data/franche-comte/franche-comte.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/lorraine-latest.osm.pbf -O gh-routing/gh-data/lorraine/lorraine.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/picardie-latest.osm.pbf -O gh-routing/gh-data/picardie/picardie.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/provence-alpes-cote-d-azur-latest.osm.pbf -O gh-routing/gh-data/paca/paca.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf -O gh-routing/gh-data/rhone-alpes/rhone-alpes.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf -O gh-routing/gh-data/idf/idf.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/nord-pas-de-calais-latest.osm.pbf -O gh-routing/gh-data/npdc/npdc.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/pays-de-la-loire-latest.osm.pbf -O gh-routing/gh-data/pays-de-la-loire/pays-de-la-loire.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/bourgogne-latest.osm.pbf -O gh-routing/gh-data/bourgogne/bourgogne.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/limousin-latest.osm.pbf -O gh-routing/gh-data/limousin/limousin.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/corse-latest.osm.pbf -O gh-routing/gh-data/corse/corse.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf -O gh-routing/gh-data/languedoc-roussillon/languedoc-roussillon.osm.pbf
+
+wget https://download.geofabrik.de/europe/france/poitou-charentes-latest.osm.pbf -O gh-routing/gh-data/poitou-charentes/poitou-charentes.osm.pbf
+
