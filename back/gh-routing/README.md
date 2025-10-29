109dad4266df4eab23497ee5905039047077eb92
Garoverr <jacques.sapin@epitech.eu>
2025-10-29 14:48:36 +0100

    ADD: ghrouting module

---
+++ back/gh-routing/README.md
@@ -0,0 +1,16 @@
+# GraphHopper
+
+## Document simple pour expliquer l'architecture et ce qui pourrait être amélioré
+
+### Architecture actuelle
+
+Chaque région possède son propre dossier dans `gh-data` avec un fichier `config.yml` pointant vers le fichier OSM de la région, un fichier `.poly` pour les limites géographiques, et (après téléchargement) un fichier `osm.pbf`.
+
+Le dossier gh-manager contient un dockerfile simple qui permet de lancer l'image docker du service graphhopper et un fichier `regions.config.json` qui orchestre toutes les informations pour les régions.
+
+Au premier lancement d'une requete de région, il y aura plus de temps car il faut du temps pour générer les fichiers de régions. Après cela, c'est casiment instantané.
+
+
+### Améliorations possibles
+
+- Centraliser les configurations dans un seul fichier JSON ou YAML pour éviter la duplication. (Script qui remplace le nom du fichier à config ou param internes ?)
--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,14 +6,15 @@ import { ConfigModule } from '@nestjs/config';
 import { User } from './database/user.entity';
 import { AddokProxyMiddleware } from './api/addok.middleware';
 import { ApicartoProxyMiddleware } from './api/apicarto.middleware';
-import { GraphhopperProxyMiddleware } from './api/graphhopper.middleware';
 import { AuthModule } from './auth/auth.module';
+import { GhRoutingModule } from './gh-routing/gh-routing.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GhRoutingModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
@@ -36,6 +37,5 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AddokProxyMiddleware).forRoutes('/addok');
     consumer.apply(ApicartoProxyMiddleware).forRoutes('/apicarto');
-    consumer.apply(GraphhopperProxyMiddleware).forRoutes('/gh');
   }
 }
