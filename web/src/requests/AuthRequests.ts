99e29b53ec352aba27452801e932d745b4e19dfb
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-11-17 16:39:51 +0200

    EDIT: change raw  url for request to env variable

--- web/src/requests/AuthRequests.ts
+++ web/src/requests/AuthRequests.ts
@@ -1,24 +1,21 @@
 import axios from "axios";
+const apiUrl = window?._env_?.API_URL;
 
 export const login = async (email: string, password: string) => {
-  const response = await axios.post("http://localhost:3000/auth/login", { email, password });
+  const response = await axios.post(apiUrl + "/auth/login", { email, password });
   return response.data;
 };
 
 export const register  = async (email: string, password: string) => {
-  const response = await axios.post("http://localhost:3000/auth/register", { email, password });
+  const response = await axios.post(apiUrl + "/auth/register", { email, password });
   return response.data;
 };
 
 export const verifyToken = async (token: string) => {
-  console.log("Token envoyé :", token);
-  const response = await axios.get("http://localhost:3000/auth/verify", {
+  const response = await axios.get(apiUrl + "/auth/verify", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-
-  console.log("AuthRequests: Token verification result:", response.data);
-  
   return response.data;
 };
--- web/src/requests/UserRequests.ts
+++ web/src/requests/UserRequests.ts
@@ -1,21 +1,22 @@
 import axios from "axios";
+const apiUrl = window?._env_?.API_URL;
 
 export const checkUserByMail = async (email: string) => {
-  const response = await axios.post("http://localhost:3000/user/check-email", { email });
+  const response = await axios.post(apiUrl + "/user/check-email", { email });
   return response.data;
 };
 
 export const getUserByMail = async (email: string) => {
-  const response = await axios.get(`http://localhost:3000/user/email/${encodeURIComponent(email)}`);
+  const response = await axios.get(apiUrl + `/user/email/${encodeURIComponent(email)}`);
   return response.data;
 };
 
 export const checkUserByToken = async (token: string) => {
-  const response = await axios.post("http://localhost:3000/user/check-email", { token });
+  const response = await axios.post(apiUrl + "/user/check-email", { token });
   return response.data;
 };
 
 export const getUserByToken = async (token: string) => {
-  const response = await axios.get(`http://localhost:3000/user/${encodeURIComponent(token)}`);
+  const response = await axios.get(apiUrl + `/user/${encodeURIComponent(token)}`);
   return response.data;
 };
