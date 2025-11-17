fc1b0dfeba5b6e824e265238adf88bd9f3e3f0db
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-11-17 13:41:43 +0200

    ADD: request for check the token

--- web/src/requests/AuthRequests.ts
+++ web/src/requests/AuthRequests.ts
@@ -9,3 +9,16 @@ export const register  = async (email: string, password: string) => {
   const response = await axios.post("http://localhost:3000/auth/register", { email, password });
   return response.data;
 };
+
+export const verifyToken = async (token: string) => {
+  console.log("Token envoyé :", token);
+  const response = await axios.get("http://localhost:3000/auth/verify", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  console.log("AuthRequests: Token verification result:", response.data);
+  
+  return response.data;
+};
