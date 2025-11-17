6d01297d9913c14efe14632b79bfebbe5076f668
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-11-17 16:36:43 +0200

    ADD: dir for guard system

---
+++ back/src/guard/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth/auth.service';
+
+export interface JwtPayload {
+  sub: number;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateUser({
+      sub: payload.sub,
+      email: payload.email,
+    });
+    
+    if (!user) {
+      throw new UnauthorizedException('Utilisateur non trouvé');
+    }
+    
+    return user;
+  }
+}
