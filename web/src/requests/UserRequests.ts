67f2d50fb09287f7be47d1befe0530efddd161e8
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-11-17 13:41:43 +0200

    ADD: request for check the token

--- web/src/requests/UserRequests.ts
+++ web/src/requests/UserRequests.ts
@@ -6,6 +6,16 @@ export const checkUserByMail = async (email: string) => {
 };
 
 export const getUserByMail = async (email: string) => {
-  const response = await axios.get(`http://localhost:3000/user/${encodeURIComponent(email)}`);
+  const response = await axios.get(`http://localhost:3000/user/email/${encodeURIComponent(email)}`);
+  return response.data;
+};
+
+export const checkUserByToken = async (token: string) => {
+  const response = await axios.post("http://localhost:3000/user/check-email", { token });
+  return response.data;
+};
+
+export const getUserByToken = async (token: string) => {
+  const response = await axios.get(`http://localhost:3000/user/${encodeURIComponent(token)}`);
   return response.data;
 };
