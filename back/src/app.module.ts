c465195d00c9fa3a5b327caa759b89c8365a1034
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2025-12-01 19:13:14 +0200

    FIX: error

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,7 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 import { User } from './database/user.entity';
 import { AddokProxyMiddleware } from './api/addok.middleware';
 import { ApicartoProxyMiddleware } from './api/apicarto.middleware';
-import { GraphhopperProxyMiddleware } from './api/graphhopper.middleware';
 import { AuthModule } from './auth/auth.module';
 import { GeoServerProxyMiddleware } from './api/geoserver.middleware';
 import { GeoModule } from './geo/geo.module';
@@ -41,7 +40,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AddokProxyMiddleware).forRoutes('/addok');
     consumer.apply(ApicartoProxyMiddleware).forRoutes('/apicarto');
-    consumer.apply(GraphhopperProxyMiddleware).forRoutes('/gh');
     consumer.apply(GeoServerProxyMiddleware).forRoutes('/geoserver');
   }
 }
