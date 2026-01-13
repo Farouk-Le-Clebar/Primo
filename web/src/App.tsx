a97aa3a303c3c29bb63031740c5f6eab65db9884
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-01-13 12:22:48 +0100

    ADD: toastr system for notifications and new system of protected route for login users

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,69 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
+// COMPONENTS
+import Layout from "./components/layouts/layout";
+import LayoutSettings from "./components/layouts/layoutSettings";
+import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
+
+// PUBLIC PAGES
 import Dashboard from "./pages/dashboard/Dashboard";
 import Projects from "./pages/projects/Projects";
 import Map from "./pages/newMap/Map";
 
+// PROTECTED PAGES
+import Profile from "./pages/profile/Profile";
+
+import EditProfile from "./pages/settings/EditProfile";
+import Notifications from "./pages/settings/Notifications";
+import Billing from "./pages/settings/billing";
+import Privacy from "./pages/settings/Privacy";
+import Security from "./pages/settings/security";
+import Subscriptions from "./pages/settings/Subscriptions";
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />  {/* / */}
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="projects" element={<Projects />} />
-      </Route>
-      <Route path="search" element={<Map />} />
-    </Routes>
+    <>
+      {/* Toaster pour les notifications */}
+      <Toaster position="top-right" reverseOrder={false}toastOptions={{
+        style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
+      <Routes>
+        {/* Routes Publiques */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+
+        {/* Routes Protégées (Redirigent vers / si pas connecté) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<LayoutSettings />}>
+            <Route path="profile" element={<Profile />} />
+
+            {/* Routes de paramètres */}
+            <Route path="settings" element={<EditProfile />} />
+            <Route path="settings/edit-profile" element={<EditProfile />} />
+            <Route path="settings/notifications" element={<Notifications />} />
+            <Route path="settings/billing" element={<Billing />} />
+            <Route path="settings/privacy" element={<Privacy />} />
+            <Route path="settings/security" element={<Security />} />
+            <Route path="settings/subscriptions" element={<Subscriptions />} />
+          </Route>
+
+        </Route>
+
+        {/* Routes publiques sans layout */}
+        <Route path="search" element={<Map />} />
+
+        {/* Catch-all redirection */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
