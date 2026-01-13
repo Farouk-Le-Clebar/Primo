a7219fa8df3bc2aa56fa92be03f2acd7deb929fd
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-01-13 12:35:04 +0100

    ADD: request to get info from the back/geoserver for exemple get buildings infos

---
+++ web/src/requests/parcel-info.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import L from 'leaflet';
+
+const apiUrl = window?._env_?.API_URL;
+
+export const getBuildingsByBbox = async (bbox: string, departement: string) => {
+  const typeNames = `primo:batiment_${departement}`;
+  
+  const params = new URLSearchParams({
+    service: 'WFS',
+    version: '2.0.0',
+    request: 'GetFeature',
+    typeName: typeNames,
+    outputFormat: 'application/json',
+    srsName: 'EPSG:4326',
+    bbox: `${bbox},EPSG:4326`
+  });
+
+  return axios
+    .get(`${apiUrl}/geoserver/primo/wfs?${params}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error fetching buildings data:", error);
+      throw error;
+    });
+};
