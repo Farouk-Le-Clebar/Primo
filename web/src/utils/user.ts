b5dbf449c6fdb307770bae400448dafa863d3874
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-01-13 12:33:41 +0100

    ADD: user fonc for global user infos like check if the info of the user are good (prbl useless)

---
+++ web/src/utils/user.ts
@@ -0,0 +1,18 @@
+import { store } from "../store/store";
+
+export const isUserInfos = () => {
+    const state = store.getState();
+    const userData = state.user.userInfo;
+
+    if (!userData || !userData.user) {
+        return false;
+    }
+
+    const user = userData.user;
+
+    if (user.firstName && user.lastName && user.email) {
+        return true;
+    }
+
+    return false;
+}
