ded813e1c0b8b525ed81f3831f755309dd92ddc9
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-01-13 12:40:55 +0100

    EDIT: add system to get info from the map

--- web/src/pages/newMap/Map.tsx
+++ web/src/pages/newMap/Map.tsx
@@ -1,26 +1,29 @@
 import { MapContainer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import InfoLayer from './components/layers/InfoLayer';
+import InfoLayer from '../../components/ParcelPanel/ParcelInfoPanel';
 import Layers from './components/Layers';
 import SearchController from './components/SearchController';
+import { ParcelleProvider } from '../../context/ParcelleContext';
 
 const Map = () => {
 
   return (
-    <div className="h-screen w-screen">
-      <MapContainer
-        center={[46.603354, 1.888334]}
-        zoom={6}
-        className="h-full w-full"
-        attributionControl={false}
-        preferCanvas={true}
-        zoomControl={false}
-      >
-        <Layers />
-        <SearchController />
-      </MapContainer>
-      <InfoLayer isVisible={false} />
-    </div>
+    <ParcelleProvider>
+      <div className="h-screen w-screen">
+        <MapContainer
+          center={[46.603354, 1.888334]}
+          zoom={6}
+          className="h-full w-full"
+          attributionControl={false}
+          preferCanvas={true}
+          zoomControl={false}
+          >
+          <Layers />
+          <SearchController />
+        </MapContainer>
+        <InfoLayer isVisible={true} />
+      </div>
+    </ParcelleProvider>
   );
 };
 
