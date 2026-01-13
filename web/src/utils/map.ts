302fcf409d9087ffc1a409ea236339b7dd3f19a4
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-01-13 12:32:56 +0100

    ADD map utils for get some func to help use the map infos

---
+++ web/src/utils/map.ts
@@ -0,0 +1,30 @@
+import type { FeatureCollection } from 'geojson';
+
+export const MIN_ZOOM_FOR_PARCELLES = 18;
+export const MIN_ZOOM_FOR_DIVISION = 15;
+export const MIN_ZOOM_FOR_CITY = 11;
+
+export const isFeatureCollection = (d: any): d is FeatureCollection =>
+    !!d && Array.isArray((d as any).features);
+
+export const style = {
+    fillColor: "#54bb8dff",
+    color: "#51b789ff",
+    weight: 2,
+};
+
+export type DataType = {
+    departements: FeatureCollection | null;
+    parcelles: FeatureCollection | null;
+    city: FeatureCollection | null;
+    divisions: FeatureCollection | null;
+};
+
+export const boundToBbox = (bounds: L.LatLngBounds) => {
+    return ([
+        bounds.getWest(),
+        bounds.getSouth(),
+        bounds.getEast(),
+        bounds.getNorth()
+    ].join(','));
+};
