c4334376a0c8c2aa1a9911b749561423c883eb05
Garoverr <jacques.sapin@epitech.eu>
2026-03-07 05:21:32 +0100

    FIX: move the projects types to a file for better reading

---
+++ web/src/types/project/projectDetail.ts
@@ -0,0 +1,18 @@
+export interface ProjectDetail {
+    id: string;
+    name: string;
+    isFavorite: boolean;
+    parcels?: Parcel[];
+    parameters?: any; // À définir
+    notes?: string;
+    createdAt: string;
+    modifiedAt: string;
+}
+
+
+export interface Parcel {
+    id: string;
+    coordinates: [number, number];
+    // le reste é à definir 
+}
+
