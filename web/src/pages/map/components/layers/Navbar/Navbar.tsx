e02a65a66436a5d0066e59d3abcee31c6a3c11c4
Louis Huguet <louis.huguet@epitech.eu>
2026-04-04 17:46:10 +0200

    FIX : Build front end

--- web/src/pages/map/components/layers/Navbar/Navbar.tsx
+++ web/src/pages/map/components/layers/Navbar/Navbar.tsx
@@ -14,6 +14,7 @@ import ProfileDropdown from "../../../../../components/navbar/components/profile
 // ICONS / LOGOS
 import PrimoLogo from "../../../../../assets/logos/logoPrimoWhite.svg?react";
 import { useMap } from "react-leaflet";
+import L from "leaflet";
 
 type NavbarProps = {
   parcelleBounds: any;
