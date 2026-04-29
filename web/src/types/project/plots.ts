3769cfa8725e1896757972a462dca6e0f1926aa3
Louis Huguet <louis.huguet@epitech.eu>
2026-04-29 14:19:10 +0200

    ADD : plot types

---
+++ web/src/types/project/plots.ts
@@ -0,0 +1,12 @@
+export type AddPlotToProjectPayload = {
+    projectId: string;
+    plotId: string;
+    plotBanId: string;
+    adress: string;
+};
+
+export type UsefullPlotData = {
+    plotId: string;
+    plotBanId: string;
+    adress: string;
+};
