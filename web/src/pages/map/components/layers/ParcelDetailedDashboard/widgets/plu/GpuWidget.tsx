2fafffdf118b91c354d692569d734bc96e93ccb0
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2026-05-09 00:11:02 +0300

    EDIT: improve layout structure in GpuWidget for better responsiveness

--- web/src/pages/map/components/layers/ParcelDetailedDashboard/widgets/plu/GpuWidget.tsx
+++ web/src/pages/map/components/layers/ParcelDetailedDashboard/widgets/plu/GpuWidget.tsx
@@ -8,19 +8,16 @@ import type { ParcelWidgetProps } from "../../types";
 
 export default function GpuWidget({ feature }: ParcelWidgetProps) {
   return (
-    <div className="font-inter w-full">
-      <Grid numItems={1} numItemsMd={3} className="gap-6 w-full relative">
-        <Col numColSpan={1}>
-          <div className="sticky top-0 h-182 flex flex-col">
-            <PluCard feature={feature} />
-          </div>
+    <div className="font-inter w-full h-full flex flex-col min-h-0">
+      <Grid numItems={1} numItemsMd={3} className="gap-6 w-full flex-1 min-h-0">
+        
+        <Col numColSpan={1} className="h-auto md:h-full">
+          <PluCard feature={feature} />
         </Col>
 
-        <Col numColSpan={1} numColSpanMd={2} className="pb-10">
-          <div className="flex flex-col gap-6">
-            <PrescriptionsCard feature={feature} />
-            <InformationsCard feature={feature} />
-          </div>
+        <Col numColSpan={1} numColSpanMd={2} className="md:h-full md:overflow-y-auto pb-4 pr-1 flex flex-col gap-6 scrollbar-custom min-h-0">
+          <PrescriptionsCard feature={feature} />
+          <InformationsCard feature={feature} />
         </Col>
 
       </Grid>
