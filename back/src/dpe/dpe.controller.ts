41662050649d6c2b32b408ac6617899d3c88124c
Louis Huguet <louis.huguet@epitech.eu>
2026-09-18 14:49:38 +0200

    EDIT: add JwtAuthGuard to DpeController for securing the getDpe endpoint

--- back/src/dpe/dpe.controller.ts
+++ back/src/dpe/dpe.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Param, BadRequestException } from '@nestjs/common';
 import { DpeService } from './dpe.service';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 
 @Controller('dpe')
 export class DpeController {
   constructor(private readonly dpeService: DpeService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('ban/:id')
   async getDpe(@Param('id') identifiantBan: string) {
     if (!identifiantBan || identifiantBan.trim().length === 0) {
