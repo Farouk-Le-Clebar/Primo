4498c14943abddc1b0d875c8ed1fc79eaf7c7a30
Louis Huguet <louis.huguet@epitech.eu>
2026-09-18 14:50:54 +0200

    EDIT: add authorization headers to Addok API requests

--- web/src/requests/addok.ts
+++ web/src/requests/addok.ts
@@ -1,20 +1,32 @@
 import axios from "axios";
 const apiUrl = window?._env_?.API_URL;
+const token = localStorage.getItem('token');
 
 export function addOkRequest(data: string) {
-    return axios.get(apiUrl + "/addok/search", { params: { q: data } })
-    .then(response => response.data)
-    .catch(() => {
-        throw new Error('Failed to fetch data from Addok API');
-    });
+    return axios
+        .get(apiUrl + "/addok/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: data
+            }
+        })
+        .then(response => response.data)
+        .catch(() => {
+            throw new Error('Failed to fetch data from Addok API');
+        });
 }
 
 export async function addOkReverseRequest(lon: number, lat: number) {
-    try {
-        const response = await axios.get(`${apiUrl}/addok/reverse?lon=${lon}&lat=${lat}`);
-        return response.data;
-    } catch (error) {
-        console.error('❌ [Addok] Failed to fetch reverse geocoding from internal API', error);
-        return null;
-    }
+    return axios
+        .get(`${apiUrl}/addok/reverse?lon=${lon}&lat=${lat}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(response => response.data)
+        .catch(() => {
+            throw new Error('Failed to fetch reverse geocoding from Addok API');
+        });
 }
